8d0e9e14646ca71412a814bcaa6f265f76eb4925
MBAir 2017 <mbair2017@192.168.2.4>
2024-07-23 22:51:11 -0700

    aaaa

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,11 +1,13 @@
 
 import { useEffect } from 'react';
-
+import { Navigate } from 'react-router-dom';
 
 export default function Inicio() {
 
   useEffect(()=>{
-      window.location.href='https://xiperafa.github.io/adminPolanco/inventario'
+      //window.location.href='https://xiperafa.github.io/adminPolanco/inventario'
+
+      <Navigate to='/adminPolanco/inventario'/>
 
 
   },[])
